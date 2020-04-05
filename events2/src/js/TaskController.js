3e3a159b8d2f9436114ce8401dd5f61cd59ed594
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-04-06 02:43:36 +0300

    events 2 done

---
+++ events2/src/js/TaskController.js
@@ -0,0 +1,49 @@
+// import TaskManager from './TaskManager';
+import Task from './Task';
+
+export default class TaskController {
+  constructor(manager) {
+    this.manager = manager;
+    this.tasks = [];
+  }
+
+  init() {
+    this.manager.addPinnedClickListener(this.onPinnedClick.bind(this));
+    this.manager.addKeyUpListener(this.onEnterPressed.bind(this));
+    this.manager.addKeyUpListener(this.filterTasks.bind(this));
+
+    this.manager.drawUi();
+    this.tasks.push(new Task('Test task', true));
+    this.tasks.push(new Task('Test task 2', true));
+    this.tasks.push(new Task('Test task 3'));
+    this.tasks.push(new Task('Test task 4'));
+
+    this.manager.redrawTasks(this.tasks);
+  }
+
+  onPinnedClick(id) {
+    const task = this.tasks.find((o) => o.id === id);
+    task.pinned = !task.pinned;
+    this.manager.redrawTasks(this.tasks);
+  }
+
+  onEnterPressed(params) {
+    const cleanText = params.text.trim();
+    if (params.keyCode === 13) {
+      if (cleanText === '') {
+        this.manager.showErrorSpan();
+        return;
+      }
+      this.tasks.push(new Task(cleanText));
+      this.manager.redrawTasks(this.tasks);
+    }
+  }
+
+  filterTasks(params) {
+    const cleanText = params.text.trim().toLowerCase();
+    if (cleanText.length) this.manager.hideErrorSpan();
+    const filteredTasks = this.tasks.filter((task) => task.pinned
+      || task.description.toLowerCase().includes(cleanText));
+    this.manager.redrawTasks(filteredTasks);
+  }
+}
