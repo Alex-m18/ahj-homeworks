cd39d47fede12a1460f307a133275cea50f17cb0
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-04-09 00:59:00 +0300

    testing fixed

--- testing/src/js/__tests__/getCardTypeByNumber.test.js
+++ testing/src/js/__tests__/getCardTypeByNumber.test.js
@@ -4,8 +4,8 @@ import cardTypes from '../cardTypes';
 test.each([
   ['mir', '2', cardTypes.mir],
   ['visa', '4', cardTypes.visa],
-  ['mastercard', '3', cardTypes.mastercard],
-  ['americanexpress', '6', cardTypes.americanexpress],
+  ['mastercard', '5', cardTypes.mastercard],
+  ['americanexpress', '3', cardTypes.americanexpress],
 ])('should return type %s', (_, input, expected) => {
   expect(getCardTypeByNumber(input)).toMatchObject(expected);
 });
