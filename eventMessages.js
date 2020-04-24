11964f4cf79244ac60443d8a8c0d6b5f45b2b5d9
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-04-24 20:26:20 +0300

    server done

---
+++ eventMessages.js
@@ -0,0 +1,31 @@
+const eventMessages = {
+  types: [
+    'action',
+    'freekick',
+    'goal',
+  ],
+  messages: [
+    { type: 'action', message: 'Идёт перемещение мяча по полю, игроки и той, и другой команды активно пытаются атаковать' },
+    { type: 'action', message: 'action1' },
+    { type: 'action', message: 'action2' },
+    { type: 'action', message: 'action3' },
+    { type: 'action', message: 'action4' },
+    { type: 'action', message: 'action5' },
+    { type: 'action', message: 'action6' },
+    { type: 'action', message: 'action7' },
+    { type: 'action', message: 'action8' },
+    { type: 'action', message: 'action9' },
+    { type: 'freekick', message: 'freekick0' },
+    { type: 'freekick', message: 'freekick1' },
+    { type: 'freekick', message: 'freekick2' },
+    { type: 'freekick', message: 'freekick3' },
+    { type: 'freekick', message: 'freekick4' },
+    { type: 'freekick', message: 'freekick5' },
+    { type: 'freekick', message: 'freekick6' },
+    { type: 'freekick', message: 'freekick7' },
+    { type: 'goal', message: 'goal0' },
+    { type: 'goal', message: 'goal1' },
+  ],
+};
+
+module.exports = eventMessages;
--- testing/src/js/getCardTypeByNumber.js
+++
@@ -1,7 +0,0 @@
-import cardTypes from './cardTypes';
-
-export default function getCardTypeByNumber(number) {
-  return Object.values(cardTypes).find(
-    (o) => o.startDigit === Number(String(number)[0]),
-  );
-}
